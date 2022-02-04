fdc2094e59ba0a32901122cb6ee445f6e70a0874
AnamarijaSoldo33 <anamarija.ams3@gmail.com>
2022-02-04 12:20:44 +0100

    installed axios

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,16 @@
 import React from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  let apiKey = "b5a2d192f3a1859ed576767031687843";
+  let city = "New York";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <form className="Search ">
       <input type="search" />
