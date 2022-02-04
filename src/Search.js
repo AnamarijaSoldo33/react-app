8e3d561d791ebf9e2dfbcc3fe4d0a16a260f2fcf
AnamarijaSoldo33 <anamarija.ams3@gmail.com>
2022-02-05 00:19:29 +0100

    update search.js

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
+  const[temperature, setTemperature]= useState(null)
   function handleResponse(response) {
     console.log(response);
   }
@@ -16,5 +17,39 @@ export default function Search() {
       <input type="search" />
       <input type="submit" className="btn btn-primary w-100" value="Search" />
     </form>
+
+    <div className="row">
+      <div className="col-6">
+        <div className="description">
+          <div className="image">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sunny"
+            />
+          </div>
+          <div className="temperature">
+            <strong>19</strong>
+            <span className="units">
+              <a href="/">C°</a>/<a href="/">F°</a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Humidity:80%</li>
+          <li>Wind:10km/h</li>
+        </ul>
+        <span>
+          {" "}
+          <a href="https://github.com/AnamarijaSoldo33/react-app">
+            Open sourced code{" "}
+          </a>{" "}
+          by Anamarija Soldo
+        </span>
+      </div>
+    </div>
+  ;
+
   );
-}
+  }
